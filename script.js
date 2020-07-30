445a5203359ed343c109cc00534677d8b0c5b173
keken <chriskendry9@gmail.com>
2020-07-30 18:58:30 +0700

    finished responsive navbar

---
+++ script.js
@@ -0,0 +1,9 @@
+function funcTog() {
+    let x = document.getElementById("myNav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+        x.preventDefault();
+    }
+}
