bbb6b6db6864d4948328153c722404af998fa302
keken <chriskendry9@gmail.com>
2020-07-31 16:48:38 +0700

    tes 2

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function funcTog() {
         x.className = "navbar";
     }
 }
+
+// // not a good result
+// $(document).ready(function() {
+//     $('#tog').click(function() {
+//         $('.nav-item').toggle(500);
+//         $('#nav-prof').text('Profile');
+//         $('#nav-prod').text('Products');
+//         $('#nav-serv').text('Services');
+//         $('#nav-cont').text('Contacts');
+//         $('#nav-team').text('Teams');
+//     })
+// })
